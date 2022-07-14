5e131b0a5d83d358d28f9e656101f08284061e9d
kewcow <kevjhoss@gmail.com>
2022-07-14 16:51:44 -0300

    version 4.2

--- main.ts
+++ main.ts
@@ -2,8 +2,8 @@ import { serve } from './deps.ts';
 import { home } from './router.ts';
 
 const handler = async (req: Request): Promise<any> => {
-  const pathname = new URL(req.url).pathname;
-  if (pathname === '/') {
+  const pattern: URLPattern = new URLPattern({pathname: '/:state'});
+  if (pattern.test(req.url)) {
     return home();
   } else {
     const file = await Deno.readFile(`${Deno.cwd()}/public${pathname}`);
