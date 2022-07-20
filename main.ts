1ebe63d11186e2a1a28042bb8f90893c785aeef3
kewcow <kevjhoss@gmail.com>
2022-07-20 17:19:30 -0300

    version de prueba ignorar

--- main.ts
+++ main.ts
@@ -2,18 +2,19 @@ import { serve } from './deps.ts';
 import { home } from './router.ts';
 
 const handler = async (req: Request): Promise<any> => {
-  const pattern: URLPattern = new URLPattern({pathname: '/:id', });
+  const pattern: URLPattern = new URLPattern({pathname: '/travels/:id', });
   const pathname = new URL(req.url).pathname;
   if (
     pathname === '/' || pathname === '/argentina' ||
     pathname === '/bolivia' || pathname === '/brasil' ||
-    pathname === '/fest' || pathname === '/reserve'
+    pathname === '/fest' || pathname === '/reserve' && pathname
   ) {
     return home();
-  } else {
-    const file = await Deno.readFile(`${Deno.cwd()}/public${pathname}`);
-    return new Response(file);
+  } else if (pattern.test(req.url)) {
+    return home();
   }
+  const file = await Deno.readFile(`${Deno.cwd()}/public${pathname}`);
+  return new Response(file);
 };
 
 const env = Deno.env.get('PORT');
